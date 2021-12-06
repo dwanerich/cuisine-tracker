1d3189a475bbc45d3303b1046c2ee5853e359b27
dwanerich <dwanerich@gmail.com>
2021-12-05 21:21:38 -0500

    created cuisines component, seed data in object, mapped thru data returning values in an H3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Cuisines } from "./components/Cuisines";
 import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="container">
     <Header title='Cuisine Tracker'/>
+    <Cuisines />
     </div>
   );
 }
--- src/components/Cuisines.js
+++ src/components/Cuisines.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
+const cuisines = [
+    {
+        id: 1,
+        text: 'pasta',
+        day: 'Dec 30th 12:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'sushi',
+        day: 'Feb 9th 2:09am',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: 'jerk chicken',
+        day: 'July 4th 7:40pm',
+        reminder: true
+    }
+]
+
 export const Cuisines = () => {
+
+
     return (
-        <div>
+        <>
+        {cuisines.map((cuisine) => {
+            return <h3>{cuisine.text}</h3>
+
+        })}
             
-        </div>
+        </>
     )
 }
