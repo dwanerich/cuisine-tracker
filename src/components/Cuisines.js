0825ba379dfe0b711f68a1343db431448a0fdd01
dwanerich <dwanerich@gmail.com>
2021-12-08 21:54:04 -0500

    added state with react hooks and moved cuisines object into useState

--- src/components/Cuisines.js
+++ src/components/Cuisines.js
@@ -1,36 +1,37 @@
-import React from 'react'
+import { useState } from "react"
 
-const cuisines = [
-    {
-        id: 1,
-        text: 'pasta',
-        day: 'Dec 30th 12:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'sushi',
-        day: 'Feb 9th 2:09am',
-        reminder: false
-    },
-    {
-        id: 3,
-        text: 'jerk chicken',
-        day: 'July 4th 7:40pm',
-        reminder: true
-    }
-]
 
 export const Cuisines = () => {
-
-
+    const [cuisines, setCuisines] = useState(
+        [
+            {
+                id: 1,
+                text: 'pasta',
+                day: 'Dec 30th 12:30pm',
+                reminder: true
+            },
+            {
+                id: 2,
+                text: 'sushi',
+                day: 'Feb 9th 2:09am',
+                reminder: false
+            },
+            {
+                id: 3,
+                text: 'jerk chicken',
+                day: 'July 4th 7:40pm',
+                reminder: true
+            }
+        ]
+    
+    )
+    
     return (
         <>
-        {cuisines.map((cuisine) => {
-            return <h3>{cuisine.text}</h3>
-
-        })}
+            {cuisines.map((cuisine) => (
+                <h3 key={cuisine.id}>{cuisine.text} | {cuisine.day} </h3>
             
-        </>
+    ))}
+        </>        
     )
 }
