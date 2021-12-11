8843268a118429fd4af90367d8cec0db28fbae1a
dwanerich <dwanerich@gmail.com>
2021-12-10 22:38:51 -0500

    moved cusines object & setState up to App.js level then pass down values to Cuisines component as prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import { Cuisines } from "./components/Cuisines";
 import { Header } from "./components/Header";
+import { useState } from "react"
+
 
 function App() {
+  const [cuisines, setCuisines] = useState(
+    [
+      {
+        id: 1,
+        text: 'pasta',
+        day: 'Dec 30th 12:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'sushi',
+        day: 'Feb 9th 2:09am',
+        reminder: false
+      },
+      {
+        id: 3,
+        text: 'jerk chicken',
+        day: 'July 4th 7:40pm',
+        reminder: true
+      }
+    ]
+
+  )
+
   return (
     <div className="container">
     <Header title='Cuisine Tracker'/>
-    <Cuisines />
+    <Cuisines cuisines={cuisines}/>
     </div>
   );
 }
--- src/components/Cuisines.js
+++ src/components/Cuisines.js
@@ -1,36 +1,11 @@
-import { useState } from "react"
 
 
-export const Cuisines = () => {
-    const [cuisines, setCuisines] = useState(
-        [
-            {
-                id: 1,
-                text: 'pasta',
-                day: 'Dec 30th 12:30pm',
-                reminder: true
-            },
-            {
-                id: 2,
-                text: 'sushi',
-                day: 'Feb 9th 2:09am',
-                reminder: false
-            },
-            {
-                id: 3,
-                text: 'jerk chicken',
-                day: 'July 4th 7:40pm',
-                reminder: true
-            }
-        ]
-    
-    )
+export const Cuisines = ({cuisines}) => {
     
     return (
         <>
             {cuisines.map((cuisine) => (
-                <h3 key={cuisine.id}>{cuisine.text} | {cuisine.day} </h3>
-            
+                <h3 key={cuisine.id}>{cuisine.text} | {cuisine.day} </h3>      
     ))}
         </>        
     )
