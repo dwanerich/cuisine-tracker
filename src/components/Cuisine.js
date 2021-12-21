8af64ff1b0a70be494f976c731dd1e06471c62ab
dwanerich <dwanerich@gmail.com>
2021-12-20 19:58:04 -0500

    p tag to cuisine list rendering in cuisine component

--- src/components/Cuisine.js
+++ src/components/Cuisine.js
@@ -3,6 +3,7 @@ export default function Cuisine({ cuisine }) {
     return (
         <div className='cuisine'>
             <h3>{cuisine.text}</h3>
+            <p>{cuisine.day}</p>
         </div>
     )
 }
